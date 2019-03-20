927ee3a94a8eaf0ef187c4455df3214f2ef9f9a0
leaf-junkie <ragsdale.jar@gmail.com>
2019-03-20 17:45:36 -0400

    Initial commit

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+-- Write SQL queries that do the following:
+DROP IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers(
+    id INT(10) NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(50) NOT NULL,
+    devoured BOOLEAN,
+    PRIMARY KEY(id)
+);
